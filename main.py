2ea8aa38a2119ab0dcb5d44fc6e89b241412433f
Arshey123 <aseemsharma1980@gmail.com>
2024-09-30 20:15:27 +0530

    added activity 3

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 # file= open("abc.txt")
 # print(file.read())
 # activity 2
+# file= open("abc.txt", "r")
+# print(file.read())
+# file= open("abc.txt", "a")
+# file.write("sky")
+# file.write("sky")
+# activity 3
 file= open("abc.txt", "r")
-print(file.read())
-file= open("abc.txt", "a")
-file.write("sky")
-file.write("sky")
+counter= 0
+content= file.read()
+list= content.split("\n")
+for i in list:
+    if i:
+        counter+=1
+print(counter)
